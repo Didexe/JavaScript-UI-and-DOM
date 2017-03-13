2866257381d53ef4589b0da2d8d7a8c9409ca604
Daniel <didexe@gmail.com>
2017-03-13 17:06:49 +0000

    Used .this

--- Topics/07. Event Model/homework/tasks/task-1.js
+++ Topics/07. Event Model/homework/tasks/task-1.js
@@ -9,7 +9,39 @@ Create a function that takes an id or DOM element and:
 
 function solve(){
   return function (selector) {
-  };
-};
+      if (typeof selector !== 'string' && !selector instanceof HTMLElement) {
+        throw new Error('Element must be a string');
+      }
+      if (typeof selector === 'string') {
+        var element = document.getElementById(selector);
+      } 
+      if (selector instanceof HTMLElement) {
+        element = selector;
+      }
+      if (element === null) {
+        throw new Error('There is no element with such ID');
+      }
 
-module.exports = solve;
+      var buttons = [].slice.apply(element.getElementsByClassName('button'));
+      var contents = [].slice.apply(element.getElementsByClassName('content'));
+      buttons.forEach(function(button) {
+        button.innerHTML = 'hide';
+        button.addEventListener('click', showHide)
+      });
+      function showHide() {
+          var nextcontent = this.nextElementSibling;
+          if (nextcontent.className !== 'button' ) {
+            while(nextcontent.className !== 'content') {
+              nextcontent = nextcontent.nextElementSibling;
+            }
+            if(nextcontent.style.visibility === 'visible' || nextcontent.style.visibility === '') {
+              nextcontent.style.visibility = 'hidden';
+              this.innerHTML = 'show';
+            } else {
+              nextcontent.style.visibility = 'visible';
+              this.innerHTML = 'hide';
+            };
+          };
+        };
+};
+};
