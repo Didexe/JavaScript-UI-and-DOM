eaad9f81535c3def32ac1fd8abdbabc410b5609e
Daniel <didexe@gmail.com>
2017-03-27 14:55:09 +0300

    Added solution by Didexe

--- Practical Exams/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
+++ Practical Exams/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
@@ -1,6 +1,29 @@
 function solve() {
     return function (selector) {
-        var template = '';		
+        var template = 
+            '<div class="events-calendar">' +
+        '<h2 class="header">Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span></h2>' +
+        '{{#days}}' +
+            '<div class="col-date">' +
+            '<div class="date">{{day}}</div>' +
+            '<div class="events">' +
+                '{{#events}}' +
+                    '{{#unless title}}' +
+                        '<div class="event none" title="{{comment}}">' +
+                            '<div class="title">Free slot</div>' +
+                        '</div>' +
+                    '{{else}}' +
+                        '<div class="event {{importance}}" title="{{comment}}">' +
+                            '<div class="title">{{title}}</div>' +
+                            '<span class="time">at: {{time}}</span>' +
+                        '</div>' +
+                    '{{/unless}}' +
+                '{{/events}}' +
+            '</div>' +
+        '</div>' +
+        '{{/days}}'
+        '</div>'
+            ;		
         document.getElementById(selector).innerHTML = template;
     };
 }
